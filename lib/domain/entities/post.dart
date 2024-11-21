5ccccf6b5006bef3646f3b1f93c29dda1a970394
Muhammad Naufal Kurniawan <naufalkurniawan605@gmail.com>
2024-11-21 18:55:26 +0700

    refactor(model): move user model to user.dart file

--- lib/domain/entities/post.dart
+++ lib/domain/entities/post.dart
@@ -1,32 +1,5 @@
-class User {
-  final int id;
-  final String fullName;
-  final String profileImg;
 
-  User({
-    required this.id,
-    required this.fullName,
-    required this.profileImg,
-  });
-
-  // Factory method to create an instance from JSON
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      id: json['id'] as int,
-      fullName: json['full_name'] as String,
-      profileImg: json['profile_img'] as String,
-    );
-  }
-
-  // Method to convert an instance to JSON
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'fullName': fullName,
-      'profileImg': profileImg,
-    };
-  }
-}
+import 'package:tomacare/domain/entities/user.dart';
 
 class Post {
   final int? id;
